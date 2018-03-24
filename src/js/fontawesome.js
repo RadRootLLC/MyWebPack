84bd4de0dbb0223b515dcec24fb586fffc5a80cc
Brandon Jones <brandon@radroot.com>
2018-03-24 19:40:18 -0400

    More work on training & boarding pages

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -3,5 +3,12 @@ const faGithub = require("@fortawesome/fontawesome-free-brands/faGithub");
 const faCogs = require("@fortawesome/fontawesome-free-solid/facogs");
 const faEdit = require("@fortawesome/fontawesome-free-regular/faedit");
 const faFacebook = require("@fortawesome/fontawesome-free-brands/faFacebook");
+const faCheckSquare = require("@fortawesome/fontawesome-free-solid/faCheckSquare");
 
-fontawesome.library.add(faGithub, faCogs, faEdit, faFacebook);
+fontawesome.library.add(
+  faGithub,
+  faCogs,
+  faEdit,
+  faFacebook,
+  faCheckSquare,
+);
