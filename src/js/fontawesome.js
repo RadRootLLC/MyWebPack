9a5590b1a4d30b85440783b1d7840bea1976cc3a
Brandon Jones <brandon@radroot.com>
2018-03-26 00:15:53 -0400

    Start on footer

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -2,6 +2,7 @@ const fontawesome = require("@fortawesome/fontawesome");
 const faFacebook = require("@fortawesome/fontawesome-free-brands/faFacebook");
 const faCheckSquare = require("@fortawesome/fontawesome-free-solid/faCheckSquare");
 const faPhone = require("@fortawesome/fontawesome-free-solid/faPhoneSquare");
+const faEmail = require("@fortawesome/fontawesome-free-solid/faAt");
 const faFax = require("@fortawesome/fontawesome-free-solid/faFax");
 const faMapMarker = require("@fortawesome/fontawesome-free-solid/faMapMarker");
 
@@ -9,6 +10,7 @@ fontawesome.library.add(
   faFacebook,
   faCheckSquare,
   faPhone,
+  faEmail,
   faFax,
   faMapMarker,
 );
