29a330f923918c3aff85baf323f196819f93aad4
Brandon Jones <brandon@radroot.com>
2018-03-27 01:36:27 -0400

    More homepage work

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -1,5 +1,6 @@
 const fontawesome = require("@fortawesome/fontawesome");
 const faFacebook = require("@fortawesome/fontawesome-free-brands/faFacebook");
+const faYouTube = require("@fortawesome/fontawesome-free-brands/faYouTube");
 const faCheckSquare = require("@fortawesome/fontawesome-free-solid/faCheckSquare");
 const faPhone = require("@fortawesome/fontawesome-free-solid/faPhoneSquare");
 const faEmail = require("@fortawesome/fontawesome-free-solid/faAt");
@@ -8,6 +9,7 @@ const faMapMarker = require("@fortawesome/fontawesome-free-solid/faMapMarker");
 
 fontawesome.library.add(
   faFacebook,
+  faYouTube,
   faCheckSquare,
   faPhone,
   faEmail,
