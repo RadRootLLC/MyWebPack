ef1cc27be85083df444f8d85178c1c7c937d0913
Brandon Jones <brandon@radroot.com>
2020-01-06 16:40:06 -0500

    Add Instagram

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -1,5 +1,6 @@
 const fontawesome = require("@fortawesome/fontawesome");
 const faFacebook = require("@fortawesome/fontawesome-free-brands/faFacebook");
+const faInstagram = require("@fortawesome/fontawesome-free-brands/faInstagram");
 const faYouTube = require("@fortawesome/fontawesome-free-brands/faYouTube");
 const faCheckSquare = require("@fortawesome/fontawesome-free-solid/faCheckSquare");
 const faPhone = require("@fortawesome/fontawesome-free-solid/faPhoneSquare");
@@ -9,6 +10,7 @@ const faMapMarker = require("@fortawesome/fontawesome-free-solid/faMapMarker");
 
 fontawesome.library.add(
   faFacebook,
+  faInstagram,
   faYouTube,
   faCheckSquare,
   faPhone,
