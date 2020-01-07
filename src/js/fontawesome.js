00d9f2924705c8f47dbba82e71731642240c3196
Brandon Jones <brandon@radroot.com>
2020-01-07 17:56:31 -0500

    Add paw and arrow-right icon

--- src/js/fontawesome.js
+++ src/js/fontawesome.js
@@ -1,18 +1,22 @@
 const fontawesome = require("@fortawesome/fontawesome");
+const faArrow = require("@fortawesome/fontawesome-free-solid/faArrowRight");
 const faFacebook = require("@fortawesome/fontawesome-free-brands/faFacebook");
 const faInstagram = require("@fortawesome/fontawesome-free-brands/faInstagram");
 const faYouTube = require("@fortawesome/fontawesome-free-brands/faYouTube");
 const faCheckSquare = require("@fortawesome/fontawesome-free-solid/faCheckSquare");
+const faPaw = require("@fortawesome/fontawesome-free-solid/faPaw");
 const faPhone = require("@fortawesome/fontawesome-free-solid/faPhoneSquare");
 const faEmail = require("@fortawesome/fontawesome-free-solid/faAt");
 const faFax = require("@fortawesome/fontawesome-free-solid/faFax");
 const faMapMarker = require("@fortawesome/fontawesome-free-solid/faMapMarker");
 
 fontawesome.library.add(
+  faArrow,
   faFacebook,
   faInstagram,
   faYouTube,
   faCheckSquare,
+  faPaw,
   faPhone,
   faEmail,
   faFax,
